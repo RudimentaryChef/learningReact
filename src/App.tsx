104593dd98ded25cc43c9ef4bff20ac37892b963
Rudimentary_Chef <akrish6@gatech.edu>
2024-08-01 21:47:10 -0400

    passes functions via prop

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import ListGroup from "./components/ListGroup";
 function App() {
+  let items = ["Adi", "YOOOOO", "BADI", "Kadi", "DEEZ NUTS"];
+  const handleSelectItem = (item: string) => {console.log(item)} 
   return (
     <div>
-      <ListGroup />
-      <ListGroup />
+      <ListGroup items = {items} heading = "Names" onSelectItem={handleSelectItem}/>
+      <ListGroup items = {items} heading = "Names" onSelectItem={handleSelectItem}/>
+
     </div>
   );
 }
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,23 +1,33 @@
 import { useState } from "react";
 import { Fragment } from "react";
-interface Props{
-    items: string[]
-    heading: string
+interface Props {
+  items: string[];
+  heading: string;
+  // function takes (item: string) =>
+  onSelectItem: (item: string) => void; // kinda like onclick
 }
-function ListGroup(props) {
-  let items = ["Adi", "YOOOOO", "BADI", "Kadi", "DEEZ NUTS"];
-  //This is a hook. Allows us to tap into built in functions. 
-  const [selectedIndex, setSelectedIndex] = useState(-1)
+function ListGroup({items, heading, onSelectItem}: Props) {
+  
+  //This is a hook. Allows us to tap into built in functions.
+  const [selectedIndex, setSelectedIndex] = useState(-1);
   //JSX doesn't have for loops
   return (
     <Fragment>
-      <h1> List </h1>
+      <h1> {heading} </h1>
       {items.length == 0 && <p> No item found</p>}
       <ul className="list-group">
-        {items.map((items, index) => (
+        {items.map((item, index) => (
           //often each item should be something like item.id
-          <li className={selectedIndex === index ? 'list-group-item active': 'list-group-item'} onClick={() => setSelectedIndex(index)} key={items}>
-            {items}
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            onClick={() => {setSelectedIndex(index), onSelectItem(item)}}
+            key={item}
+          >
+            {item}
           </li>
         ))}
       </ul>
