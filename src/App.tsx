76c832b4010bf1a8124f188d75ca33943465cdfb
Rudimentary_Chef <akrish6@gatech.edu>
2024-08-13 16:23:32 -0400

    adds a listGroup folder in components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
+import { useState } from "react";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
+//By default complier will look at the index file
 import ListGroup from "./components/ListGroup";
+
 function App() {
-  let items = ["Adi", "YOOOOO", "BADI", "Kadi", "DEEZ NUTS"];
-  const handleSelectItem = (item: string) => {console.log(item)} 
+  const [state, setState] = useState(false);
+  let text = "I am a happy little button!";
+  let alertText = "I AM AN ALERT";
+  // Sample data to pass as props
+  const items = ["Item 1", "Item 2", "Item 3", "Item 4"];
+  const heading = "List of Items";
+
+  // Function to handle item selection
+  const handleSelectItem = (item: string) => {
+    console.log("Selected item:", item);
+  };
+
   return (
     <div>
-      <ListGroup items = {items} heading = "Names" onSelectItem={handleSelectItem}/>
-      <ListGroup items = {items} heading = "Names" onSelectItem={handleSelectItem}/>
-
+      <ListGroup
+        items={items}
+        heading={heading}
+        onSelectItem={handleSelectItem}
+      />
     </div>
   );
 }
--- src/components/ListGroup/ListGroup.tsx
+++ src/components/ListGroup/ListGroup.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Fragment } from "react";
+import './ListGroup.css'
 interface Props {
   items: string[];
   heading: string;
