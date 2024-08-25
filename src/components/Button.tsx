b449150337984d0184ca3fad7d2be28a49140398
Rudimentary_Chef <akrish6@gatech.edu>
2024-08-25 15:18:26 -0400

    styels the button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,7 +6,19 @@ interface Props {
 const Button = ({ children, onClick }: Props) => {
   return (
     <div>
-      <button type="button" className="btn btn-primary" onClick={onClick}>
+      <button
+        style={{
+          backgroundColor: "blue",
+          color: "white",
+          padding: "10px 20px",
+          border: "none",
+          borderRadius: "5px",
+          cursor: "pointer",
+        }}
+        type="button"
+        className="btn btn-primary"
+        onClick={onClick}
+      >
         {children}
       </button>
     </div>
